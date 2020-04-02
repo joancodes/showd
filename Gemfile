6891209f2457cfb0f9089b22f0d6a7b5b2029a8b
Joan Njuguna <njugunajoan35@gmail.com>
2020-04-02 23:30:08 +0300

    Add Devise and User models to App

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.0'
 gem 'simple_form'
 gem 'bootstrap-sass'
 gem 'jquery-rails'
+gem 'devise'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_02_190324) do
+ActiveRecord::Schema.define(version: 2020_04_02_202541) do
 
   create_table "plays", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 2020_04_02_190324) do
     t.string "director"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
