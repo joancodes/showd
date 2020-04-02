ef3de78792d574a9eecc0a805d106d2b2c2b0260
Joan Njuguna <njugunajoan35@gmail.com>
2020-04-02 23:13:36 +0300

    Edit and Delete actions and view files

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -2,7 +2,7 @@ class PlaysController < ApplicationController
     before_action :find_play, only: [:show, :edit, :update, :destroy]
 
     def index
-    @plays = Play.all.order("created_at DESC")
+        @plays = Play.all.order("created_at DESC")
     end
 
     def show
@@ -21,6 +21,26 @@ class PlaysController < ApplicationController
         else
             render 'new'
         end 
+    end
+
+    def edit
+
+    end
+
+    def update
+        if @play.update(play_params)
+            redirect_to play_path(@play)
+        else 
+            render 'edit'
+        end 
+
+    end
+
+    def destroy
+
+        @play.destroy
+        redirect_to root_path
+
     end
 
 
