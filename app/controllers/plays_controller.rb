6e89836671511ac93855f23c20775afa3c80b2ad
Joan Njuguna <njugunajoan35@gmail.com>
2020-04-02 22:35:02 +0300

    Add New and Create Actions with View Files

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -3,4 +3,17 @@ class PlaysController < ApplicationController
     def index
     
     end
+
+    def new 
+        @play = Play.new
+    end 
+
+    def create
+        @play = Play.new(play_params)
+    end
+
+    private
+      def play_params
+        params.require(:play).permit(:title, :description, :director)
+      end
 end
