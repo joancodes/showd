65f6ba1a505b50b7e2afb60ac7063b58492a9b04
nyamburanjuguna <njugunajoan35@gmail.com>
2020-04-27 18:09:33 +0300

    aws

--- Gemfile
+++ Gemfile
@@ -13,6 +13,8 @@ gem 'devise'
 
 gem 'active_storage_validations'
 
+gem "aws-sdk-s3", require: false
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4', '>= 5.2.4.2'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
@@ -40,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
